aa634a273b0aab878ea5914d6e18f67f1df4b52f
MozartOnWeb <mozartontheweb@gmail.com>
2024-12-06 16:19:22 +0000

    installed gifted chart

--- components/currency-sheet.tsx
+++ components/currency-sheet.tsx
@@ -1,15 +1,19 @@
-import React, { useCallback, useMemo, forwardRef } from "react";
-import { StyleSheet, View, Text } from "react-native";
+import React, { useCallback, useMemo, forwardRef, useState } from "react";
+import { StyleSheet, View, Text, TouchableWithoutFeedback } from "react-native";
 import {
   BottomSheetView,
   BottomSheetModal,
   BottomSheetModalProvider,
   BottomSheetBackdrop,
   BottomSheetBackdropProps,
+  useBottomSheetModal,
 } from "@gorhom/bottom-sheet";
 
-// type for the data items
-type DataItem = string;
+import Animated, {
+  useSharedValue,
+  useAnimatedStyle,
+  withTiming,
+} from "react-native-reanimated";
 
 // type for the props
 export interface CurrenciesSheetProps {
@@ -18,7 +22,10 @@ export interface CurrenciesSheetProps {
 
 const CurrencySheet = forwardRef<BottomSheetModal, CurrenciesSheetProps>(
   (props, ref: any) => {
+    const [activeCurrency, setActiveCurrency] = useState("XOF");
+
     const snapPoints = useMemo(() => ["82%"], []);
+    const currencies = useMemo(() => ["XOF", "EUR"], []);
 
     // callbacks
     const renderBackdrop = useCallback((props: BottomSheetBackdropProps) => {
@@ -34,8 +41,33 @@ const CurrencySheet = forwardRef<BottomSheetModal, CurrenciesSheetProps>(
 
     const handleSheetChanges = useCallback((index: number) => {
       console.log("handleSheetChanges", index);
+
+      if (index === -1) {
+        setActiveCurrency("XOF");
+      }
     }, []);
 
+    const scale = useSharedValue(1);
+
+    // Animated style
+    const animatedStyle = useAnimatedStyle(() => ({
+      transform: [{ scale: scale.value }],
+    }));
+
+    const handlePressIn = () => {
+      scale.value = withTiming(0.95, { duration: 200 });
+    };
+
+    const handlePressOut = () => {
+      scale.value = withTiming(1, { duration: 200 });
+    };
+
+    const handleSelectCurrency = useCallback(
+      (currency: string) => {
+        setActiveCurrency(currency);
+      },
+      [activeCurrency]
+    );
     return (
       <BottomSheetModalProvider>
         <BottomSheetModal
@@ -51,7 +83,46 @@ const CurrencySheet = forwardRef<BottomSheetModal, CurrenciesSheetProps>(
           {...props}
         >
           <BottomSheetView style={styles.contentContainerStyle}>
-            <Text>Awesome 🎉</Text>
+            <View style={styles.header}>
+              <View style={styles.currencyInfosWrapper}>
+                <View
+                  style={[
+                    styles.currencySymbolWrapper,
+                    { backgroundColor: props.color },
+                  ]}
+                >
+                  <Text style={styles.currencySymbol}>USD</Text>
+                </View>
+                <Text style={styles.currencyName}>United States Dollar</Text>
+              </View>
+
+              <View style={styles.currenciesSelectorWrapper}>
+                {currencies.map((currency, index) => (
+                  <TouchableWithoutFeedback
+                    onPressIn={handlePressIn}
+                    onPressOut={handlePressOut}
+                    onPress={() => handleSelectCurrency(currency)}
+                    key={index}
+                  >
+                    <Animated.View
+                      style={
+                        activeCurrency === currency
+                          ? [
+                              styles.currencyWrapper,
+                              { backgroundColor: props.color },
+                              animatedStyle,
+                            ]
+                          : styles.currencyWrapperInactive
+                      }
+                    >
+                      <Text style={styles.currency}>{currency}</Text>
+                    </Animated.View>
+                  </TouchableWithoutFeedback>
+                ))}
+              </View>
+
+              <View style={styles.currencyChartWrapper}></View>
+            </View>
           </BottomSheetView>
         </BottomSheetModal>
       </BottomSheetModalProvider>
@@ -71,6 +142,67 @@ const styles = StyleSheet.create({
   },
   backgroundStyle: {
     backgroundColor: "#F7ECC9",
+  },
+  header: {
+    paddingTop: 10,
+  },
+  currencyInfosWrapper: {
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  currencySymbolWrapper: {
+    justifyContent: "center",
+    alignItems: "center",
+    width: 55,
+    height: 55,
+    borderRadius: 100,
+  },
+  currencySymbol: {
+    fontWeight: "bold",
+    color: "#0d1321",
+  },
+  currencyName: {
+    color: "#0d1321",
+    fontSize: 16,
+    opacity: 0.4,
+    marginTop: 10,
+  },
+  currenciesSelectorWrapper: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    gap: 10,
+    marginTop: 20,
+    borderWidth: 1,
+    borderColor: "rgba(0, 0, 0, 0.05)",
+    borderRadius: 15,
+    paddingHorizontal: 10,
+    paddingVertical: 5,
+    width: "65%",
+    alignSelf: "center",
+  },
+  currencyWrapper: {
+    width: "50%",
+    paddingVertical: 10,
+    borderRadius: 10,
+    //backgroundColor: "#89E3A3",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  currencyWrapperInactive: {
+    width: "50%",
+    paddingVertical: 10,
+    borderRadius: 10,
+    backgroundColor: "#f7ecc993",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  currency: {
+    color: "#0d1321",
+    fontSize: 16,
+  },
+  currencyChartWrapper: {
+    flex: 1,
   },
 });
 
