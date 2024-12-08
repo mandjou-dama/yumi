2a5d4935bec996ac193e2bd6d37533c0e67514e5
MozartOnWeb <mozartontheweb@gmail.com>
2024-12-08 20:22:14 +0000

    working

--- components/chart.tsx
+++ components/chart.tsx
@@ -1,8 +1,9 @@
-import React, { useState } from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React, { useCallback, useState } from "react";
+import { View, Text, StyleSheet, TouchableWithoutFeedback } from "react-native";
 import { LineGraph, GraphPoint } from "react-native-graph";
-import timeseries from "../data/timeseries.json";
 import * as Haptics from "expo-haptics";
+
+import timeseries from "../data/timeseries.json";
 
 const Chart = ({ color }: { color: string | undefined }) => {
   const graphData: GraphPoint[] = Object.entries(timeseries.results["XAF"]).map(
@@ -14,20 +15,43 @@ const Chart = ({ color }: { color: string | undefined }) => {
     }
   );
 
+  const [activeTimeline, setActiveTimeline] = useState<{ range: string }>({
+    range: "1D",
+  });
+
   const [selectedPoint, setSelectedPoint] = useState<GraphPoint>(
     graphData[graphData.length - 1]
   );
 
-  const onPointSelected = (point: GraphPoint) => {
+  const onPointSelected = useCallback((point: GraphPoint) => {
     setSelectedPoint(point);
-  };
-
-  const hapticFeedback = () => {
     Haptics.selectionAsync();
+  }, []);
+
+  const graphTimelineData = [
+    {
+      range: "1D",
+    },
+    {
+      range: "1W",
+    },
+    {
+      range: "1M",
+    },
+    {
+      range: "3M",
+    },
+    {
+      range: "1Y",
+    },
+  ];
+
+  const handleSelectTimeline = (range: { range: string }) => {
+    setActiveTimeline(range);
   };
 
   return (
-    <View>
+    <View style={styles.container}>
       <View style={styles.graphHeader}>
         <Text style={styles.graphAmount}>
           ${selectedPoint?.value.toFixed(2)}
@@ -42,7 +66,7 @@ const Chart = ({ color }: { color: string | undefined }) => {
         </Text>
       </View>
 
-      <View style={{ alignItems: "center", marginTop: 15 }}>
+      <View style={{ marginTop: 15 }}>
         <LineGraph
           style={[styles.graph, StyleSheet.absoluteFill]}
           points={graphData}
@@ -50,7 +74,6 @@ const Chart = ({ color }: { color: string | undefined }) => {
           color={color || "#F7ECC9"}
           enablePanGesture={true}
           onPointSelected={onPointSelected}
-          onGestureStart={hapticFeedback}
           enableFadeInMask={true}
           enableIndicator={true}
           indicatorPulsating
@@ -58,26 +81,64 @@ const Chart = ({ color }: { color: string | undefined }) => {
           horizontalPadding={25}
         />
       </View>
+
+      <View style={styles.graphTimelineWrapper}>
+        {graphTimelineData.map((item, index) => (
+          <TouchableWithoutFeedback
+            onPress={() => handleSelectTimeline(item)}
+            key={index}
+          >
+            <View
+              style={
+                activeTimeline.range === item.range
+                  ? [styles.graphTimelineItemActive]
+                  : styles.graphTimelineItem
+              }
+            >
+              <Text
+                style={
+                  activeTimeline.range === item.range
+                    ? {
+                        textAlign: "center",
+                        fontSize: 10,
+                        color: color,
+                        fontWeight: "bold",
+                      }
+                    : { textAlign: "center", fontSize: 10 }
+                }
+              >
+                {item.range}
+              </Text>
+            </View>
+          </TouchableWithoutFeedback>
+        ))}
+      </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    height: "100%",
+    position: "relative",
+  },
   graphHeader: {
     justifyContent: "center",
     alignItems: "center",
     marginTop: 10,
     overflow: "visible",
-    height: 70,
+    height: 50,
     width: "100%",
   },
   graph: {
     width: "100%",
     height: 280,
     overflow: "visible",
+    position: "relative",
   },
   graphAmount: {
-    fontSize: 24,
+    fontSize: 22,
     fontWeight: "bold",
     width: "100%",
     textAlign: "center",
@@ -87,6 +148,30 @@ const styles = StyleSheet.create({
     textTransform: "capitalize",
     width: "100%",
     textAlign: "center",
+  },
+  graphTimelineWrapper: {
+    width: "100%",
+    height: 10,
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center",
+    position: "absolute",
+    top: 410,
+  },
+  graphTimelineItem: {
+    width: 35,
+    height: 35,
+    borderRadius: 10,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  graphTimelineItemActive: {
+    width: 35,
+    height: 35,
+    borderRadius: 10,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#0d1321",
   },
 });
 
